899217f9f644304182cce8af55e686847480b359
Bat-Erdene <baterdene.mvp@gmail.com>
2021-04-13 00:24:57 +0800

    Coming Soon

---
+++ js/scripts.js
@@ -0,0 +1,36 @@
+(function($) {
+    $(window).on('load', function() {
+        $('.fade-in').css({ position: 'relative', opacity: 0, top: -14 });
+        setTimeout(
+            function() {
+                $('#preload-content').fadeOut(400, function() {
+                    $('#preload').fadeOut(800);
+                    setTimeout(
+                        function() {
+                            $('.fade-in').each(function(index) {
+                                $(this).delay(400 * index).animate({ top: 0, opacity: 1 }, 800);
+                            });
+                        },
+                        800
+                    );
+                });
+            },
+            400
+        );
+    });
+
+    $(document).ready(function() {
+        // Countdown timer
+        // Change the launchDay according to your needs.
+        // The month ranges from 0 to 11. I specify the month from 1 to 12 and manually subtract the 1.
+        // Thus the launchDay below denotes 7 December, 2017.
+        var launchDay = new Date(2021, 4 - 1, 14, 15);
+        $('#timer').countdown({
+            until: launchDay,
+            format: 'dHMS'
+        });
+
+        // Background image
+        $.backstretch('images/cover.png');
+    });
+})(jQuery);
