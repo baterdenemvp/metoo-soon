e6fdb59097b329de09824c2d6aed9fb128b76268
Bat-Erdene <baterdene.mvp@gmail.com>
2021-04-14 10:47:44 +0800

    Updade Countdown

--- js/scripts.js
+++ js/scripts.js
@@ -24,7 +24,7 @@
         // Change the launchDay according to your needs.
         // The month ranges from 0 to 11. I specify the month from 1 to 12 and manually subtract the 1.
         // Thus the launchDay below denotes 7 December, 2017.
-        var launchDay = new Date(2021, 4 - 1, 14, 15);
+        var launchDay = new Date(2021, 4 - 1, 15, 15);
         $('#timer').countdown({
             until: launchDay,
             format: 'dHMS'
